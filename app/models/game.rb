e18d2b626a64e362b71754be67509cdff485b5e5
Erin Francis <ezzzaf@gmail.com>
2017-06-23 08:07:03 +1000

    Fix method name

--- app/models/game.rb
+++ app/models/game.rb
@@ -99,7 +99,7 @@ class Game < ActiveRecord::Base
   end
 
   def score_game
-    trick_count = bid_winner.team_won_tricks
+    trick_count = bid_winner.team_won_tricks_count
 
     if trick_count >= highest_bid.tricks
       score = highest_bid.score
